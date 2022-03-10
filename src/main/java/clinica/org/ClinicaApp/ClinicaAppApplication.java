261c8eaa92c989b54e6e3e4375db875b7bfc0dfc
Olvera25 <loov1625@gmail.com>
2022-03-10 16:47:40 -0600

    Commit de creacion de proyecto

---
+++ src/main/java/clinica/org/ClinicaApp/ClinicaAppApplication.java
@@ -0,0 +1,13 @@
+package clinica.org.ClinicaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaAppApplication.class, args);
+	}
+
+}
