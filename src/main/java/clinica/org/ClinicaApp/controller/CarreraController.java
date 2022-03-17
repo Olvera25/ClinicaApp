26c76777ac2e9c9e9250a2608efaed06db165d63
Eber <eber.mtzg.dev@gmail.com>
2022-03-17 16:54:29 -0600

    Creación de templates para carrera

--- src/main/java/clinica/org/ClinicaApp/controller/CarreraController.java
+++ src/main/java/clinica/org/ClinicaApp/controller/CarreraController.java
@@ -1,14 +1,24 @@
 package clinica.org.ClinicaApp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import clinica.org.ClinicaApp.entity.Carrera;
+import clinica.org.ClinicaApp.service.CarreraService;
+
 @RestController
 @RequestMapping("/clinica")
 public class CarreraController {
+	@Autowired
+	CarreraService service;
+	
 	@GetMapping("/Carrera")
-	public void listar() {
+	public List<Carrera> listar() {
 		System.out.println("Entrando a ...");
+		return service.listarCarrera();
 	}
 }
